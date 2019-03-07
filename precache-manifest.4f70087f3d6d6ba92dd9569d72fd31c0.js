8bd3b6884be4d3438733e0a3e839c03b11014b4f
Thomas <1767941+tthomasgg@users.noreply.github.com>
2019-03-06 23:06:25 -0300

    Updates

---
+++ precache-manifest.4f70087f3d6d6ba92dd9569d72fd31c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "1a6cf941b9ba4e79ebd9",
+    "url": "/contactmanager/static/js/main.1a6cf941.chunk.js"
+  },
+  {
+    "revision": "d8c5086063f8616b85f4",
+    "url": "/contactmanager/static/js/2.d8c50860.chunk.js"
+  },
+  {
+    "revision": "1a6cf941b9ba4e79ebd9",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "d8c5086063f8616b85f4",
+    "url": "/contactmanager/static/css/2.50722e3f.chunk.css"
+  },
+  {
+    "revision": "24298f95483bce57e7871619997414c2",
+    "url": "/contactmanager/index.html"
+  }
+];
